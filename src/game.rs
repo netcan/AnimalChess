71dda6b339c3f12f960e21c13b2ad88e6243006b
netcan <1469709759@qq.com>
2020-05-31 11:30:53 +0800

    refactor game

--- src/game.rs
+++ src/game.rs
@@ -1,31 +1,79 @@
-use sdl2::render::{TextureCreator, WindowCanvas};
+use sdl2::render::{Texture, WindowCanvas};
 use sdl2::rect::Rect;
+use sdl2::video::Window;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::EventPump;
+use sdl2::image::LoadTexture;
+use std::time::Duration;
 use crate::chess::*;
 
 const ROW_NUM: usize = 10;
 const COL_NUM: usize = 9;
 
 pub struct Game {
-    board: [[Box<Chess>; COL_NUM]; ROW_NUM],
+    chesses: [[Box<Chess>; COL_NUM]; ROW_NUM],
+    board: Texture,
+    canvas: WindowCanvas,
+    event_pump: EventPump
 }
 
 impl Game {
-    pub fn new<T>(texture_creator: &TextureCreator<T>) -> Self {
-        let game = Game {
-            board: array![
-                array![Box::new(Chess::new(get_chess_id(RED, KING), texture_creator)); COL_NUM];
-            ROW_NUM]
+    pub fn new(window: Window, event_pump: EventPump) -> Self {
+        let canvas = window.into_canvas()
+            .present_vsync()
+            .build().expect("could not make a canvas");
+
+        let texture_creator = canvas.texture_creator();
+
+        let mut game = Game {
+            chesses: array![
+                array![Box::new(Chess::new(get_chess_id(RED, KING), &texture_creator)); COL_NUM];
+            ROW_NUM],
+            board: texture_creator.load_texture("assets/board.jpg").unwrap(),
+            canvas,
+            event_pump
         };
+        game.canvas.set_logical_size(
+            game.board.query().width,
+            game.board.query().height
+        ).expect("set logical_size failed");
         game
     }
 
-    pub fn render_chess(&self, canvas: &mut WindowCanvas) {
+    pub fn render(&mut self) -> Result<(), String> {
+        self.canvas.clear();
+        self.canvas.copy(&self.board, None, None)?;
         for i in 0..ROW_NUM {
             for j in 0..COL_NUM {
-                canvas.copy((self.board[i][j].texture.as_ref().unwrap()), None, Rect::new(
+                self.canvas.copy(self.chesses[i][j].texture.as_ref().unwrap(), None, Rect::new(
                     j as i32 * 57, i as i32 * 57, 57, 57
-                ));
+                ))?;
             }
         }
+        self.canvas.present();
+        Ok(())
+    }
+
+    pub fn run(&mut self) -> Result<(), String> {
+        'running: loop {
+            // handle event
+            for event in self.event_pump.poll_iter() {
+                match event {
+                    Event::Quit {..} |
+                        Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                            break 'running
+                        },
+                    _ => {}
+                }
+            }
+            // update
+            self.render()?;
+
+            // time management
+            ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        }
+
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,7 @@ extern crate array_macro;
 mod game;
 mod chess;
 
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
-use sdl2::rect::Rect;
-use sdl2::render::{WindowCanvas, Texture};
 use game::*;
-use sdl2::image::{self, LoadTexture, InitFlag};
-use std::time::Duration;
 
 const WINDOW_WIDTH: u32 = 521;
 const WINDOW_HEIGHT: u32 = 577;
@@ -24,39 +18,9 @@ fn main() -> Result<(), String> {
         .allow_highdpi()
         .build().expect("could not initialize video subsystem");
 
+    let mut game = Game::new(windows, sdl_ctx.event_pump()?);
 
-    let mut canvas = windows.into_canvas()
-        .present_vsync()
-        .build().expect("could not make a canvas");
-    canvas.set_logical_size(WINDOW_WIDTH, WINDOW_HEIGHT).expect("set logical_size failed");
-
-    let _image_ctx = image::init(InitFlag::JPG)?;
-    let texture_creator = canvas.texture_creator();
-    let board = texture_creator.load_texture("assets/board.jpg")?;
-
-    let mut game = Game::new(&texture_creator);
-
-    let mut event_pump = sdl_ctx.event_pump()?;
-    'running: loop {
-        // handle event
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running
-                },
-                _ => {}
-            }
-        }
-        // update
-        canvas.clear();
-        canvas.copy(&board, None, None)?;
-        game.render_chess(&mut canvas);
-        canvas.present();
-
-        // time management
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
-    }
+    game.run()?;
 
     Ok(())
 }
