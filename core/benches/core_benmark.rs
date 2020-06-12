630a224dfc39488b2c75e0b3e0f32530c6b1b0e6
netcan <1469709759@qq.com>
2020-06-12 22:33:17 +0800

    self play benmark

--- core/benches/core_benmark.rs
+++ core/benches/core_benmark.rs
@@ -1,7 +1,5 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use animal_chess_core::board::*;
-use animal_chess_core::chess::*;
-use core::time::Duration;
 use rand::seq::SliceRandom;
 
 #[inline]
@@ -24,5 +22,31 @@ fn gen_and_move_benmark(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, gen_and_move_benmark);
+#[inline]
+fn self_play() -> usize {
+    let mut board = Board::new();
+    let mut step = 0;
+    loop {
+        let steps = board.generate_all_steps();
+        if steps.is_empty() { break; }
+        board.move_chess(*steps.choose(&mut rand::thread_rng()).unwrap());
+        step += 1;
+    }
+
+    step
+}
+
+fn self_play_benmark(c: &mut Criterion) {
+    let mut steps = 0;
+    let mut count = 0;
+    c.bench_function("self_play_benmark", |b| {
+        b.iter(|| {
+            steps += self_play();
+            count += 1;
+        });
+    });
+    println!("average {}/{}={} steps to end", steps, count, steps / count);
+}
+
+criterion_group!(benches, gen_and_move_benmark, self_play_benmark);
 criterion_main!(benches);
